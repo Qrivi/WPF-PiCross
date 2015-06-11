ec6bc199a2044d0f2e2e55b71e0967b0decfa100
Frederic Vogels <frederic.vogels@gmail.com>
2015-06-11 22:07:25 +0200

    Removed Solver class

--- PiCross/Domain/Game/Constraints.cs
+++ PiCross/Domain/Game/Constraints.cs
@@ -20,7 +20,73 @@ namespace PiCross.Game
 
         public IEnumerable<Slice> Generate( int sliceLength )
         {
-            return Solver.GeneratePatterns( sliceLength, values ).Select( x => new Slice( x ) );
+            return GeneratePatterns( sliceLength, values ).Select( x => new Slice( x ) );
+        }
+
+        private static IEnumerable<ISequence<int>> GenerateIntegers( int count, int sum )
+        {
+            // TODO Validate arguments
+
+            if ( count == 0 )
+            {
+                if ( sum == 0 )
+                {
+                    return new List<ISequence<int>> { Sequence.CreateEmpty<int>() };
+                }
+                else
+                {
+                    return Enumerable.Empty<ISequence<int>>();
+                }
+            }
+            else
+            {
+                // TODO Possible optimization: create separate sequence prefixer class
+                return from i in Enumerable.Range( 0, sum + 1 )
+                       from tail in GenerateIntegers( count - 1, sum - i )
+                       let prefix = Sequence.FromItems( i )
+                       select prefix.Concatenate( tail );
+            }
+        }
+
+        private static IEnumerable<ISequence<int>> GenerateSpacings( int length, int constraintCount, int constraintSum )
+        {
+            // TODO Validate arguments
+
+            var spacingCount = constraintCount + 1;
+            var spacingSum = length - constraintSum - Math.Max( 0, constraintCount - 1 );
+
+            var numbers = GenerateIntegers( spacingCount, spacingSum );
+            var deltas = Sequence.FromFunction( spacingCount, i => i == 0 || i == spacingCount - 1 ? 0 : 1 );
+
+            return numbers.Select( ns => ns.ZipWith( deltas, ( x, y ) => x + y ) );
+        }
+
+        private static IEnumerable<ISequence<SquareState>> GeneratePatterns( int totalSize, ISequence<int> constraints )
+        {
+            if ( totalSize < 0 )
+            {
+                throw new ArgumentOutOfRangeException( "totalSize" );
+            }
+            else if ( constraints == null )
+            {
+                throw new ArgumentNullException( "constraints" );
+            }
+            else
+            {
+                var constraintCount = constraints.Length;
+                var constraintSum = constraints.Items.Sum();
+
+                var spacings = GenerateSpacings( totalSize, constraintCount: constraintCount, constraintSum: constraintSum );
+
+                var blocks = constraints.Map( n => Sequence.Repeat( n, SquareState.FILLED ) );
+
+                foreach ( var spacing in spacings )
+                {
+                    var spaces = spacing.Map( n => Sequence.Repeat( n, SquareState.EMPTY ) );
+
+                    yield return spaces.Intersperse( blocks ).Flatten();
+                }
+            }
         }
     }
 }
