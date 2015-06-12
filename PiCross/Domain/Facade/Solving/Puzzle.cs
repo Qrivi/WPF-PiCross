b255536beff93b3a4b157d3ae18ed7f2bcab5162
Frederic Vogels <frederic.vogels@gmail.com>
2015-06-12 14:40:34 +0200

    Added facade interfaces

---
+++ PiCross/Domain/Facade/Solving/Puzzle.cs
@@ -0,0 +1,40 @@
+﻿using PiCross.DataStructures;
+using PiCross.Game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiCross.Facade.Solving
+{
+    public interface IPuzzle
+    {
+        int Width { get; }
+
+        int Height { get; }
+
+        IPuzzleSquare this[Vector2D position] { get; }
+
+        IPuzzleConstraints ColumnConstraints( int x );
+
+        IPuzzleConstraints RowConstraints( int x );
+    }
+
+    public interface IPuzzleSquare
+    {
+        ICell<Square> Contents { get; }
+    }
+
+    public interface IPuzzleConstraints
+    {
+        IList<IPuzzleConstraint> Constraints { get; }
+    }
+
+    public interface IPuzzleConstraint
+    {
+        int Value { get; }
+
+        ICell<bool> Satisfied { get; }
+    }
+}
