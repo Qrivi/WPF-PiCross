29a40376555e7aa962ae32bf642ff47e904583a8
Frederic Vogels <frederic.vogels@gmail.com>
2015-06-13 00:20:52 +0200

    Renamed ConstraintsSatisfiedSuffixLengthTests.cs to Constraints_SatisfiedSuffixLength_Tests.cs

--- PiCross/Tests/Constraints_SatisfiedSuffixLength_Tests.cs
+++ PiCross/Tests/Constraints_SatisfiedSuffixLength_Tests.cs
@@ -5,7 +5,7 @@ using PiCross.Game;
 namespace PiCross.Tests
 {
     [TestClass]
-    public class ConstraintsSatisfiedSuffixLengthTests
+    public class Constraints_SatisfiedSuffixLength_Tests
     {
         [TestMethod]
         [TestCategory( "Constraints" )]
