945943da705fe943231762b510beed7547b09f29
Frederic Vogels <frederic.vogels@gmail.com>
2015-06-16 20:28:16 +0200

    Added signals

---
+++ PiCross/Domain/DataStructures/Signal.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PiCross.Cells;
+
+namespace PiCross.DataStructures
+{
+    public interface ISignal
+    {
+        void Send();
+    }
+
+    public class SignalFactory<T>
+    {
+        private readonly Cell<T> cell;
+
+        public SignalFactory( Cell<T> cell )
+        {
+            this.cell = cell;
+        }
+
+        public SignalFactory( T initialCellContents = default(T) )
+            : this( PiCross.Cells.Cell.Create<T>( initialCellContents ) )
+        {
+            // NOP
+        }
+
+        public Cell<T> Cell
+        {
+            get
+            {
+                return cell;
+            }
+        }
+
+        public ISignal CreateSignal( T value )
+        {
+            return new Signal( cell, value );
+        }
+
+        private class Signal : ISignal
+        {
+            private readonly T value;
+
+            private readonly Cell<T> cell;
+
+            public Signal( Cell<T> cell, T value )
+            {
+                this.cell = cell;
+                this.value = value;
+            }
+
+            public void Send()
+            {
+                cell.Value = value;
+            }
+        }
+    }
+}
