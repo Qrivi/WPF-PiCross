e7c067e27bdb77a2e6af7e40e850e1786a705264
Frederic Vogels <frederic.vogels@gmail.com>
2015-06-20 10:44:35 +0200

    Added Editing facade interfaces

---
+++ PiCross/Domain/Facade/Editing/IPuzzleEditor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PiCross.Cells;
+using PiCross.DataStructures;
+using PiCross.Game;
+
+namespace PiCross.Facade.Editing
+{
+    public interface IPuzzleEditor
+    {
+        int Width { get; }
+
+        int Height { get; }
+
+        IPuzzleEditorSquare this[Vector2D position] { get; }
+
+        ISequence<IPuzzleEditorConstraints> ColumnConstraints { get; }
+
+        ISequence<IPuzzleEditorConstraints> RowConstraints { get; }
+    }
+
+    public interface IPuzzleEditorSquare
+    {
+        Cell<Square> Contents { get; }
+
+        Vector2D Position { get; }
+    }
+
+    public interface IPuzzleEditorConstraints
+    {
+        Cell<ISequence<int>> Values { get; }
+    }
+}
