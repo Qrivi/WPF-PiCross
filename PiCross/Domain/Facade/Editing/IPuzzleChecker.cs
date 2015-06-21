374d321c9eaa44b1543042a0f7a327373b05d160
Frederic Vogels <frederic.vogels@gmail.com>
2015-06-21 14:34:31 +0200

    Added IPuzzleChecker

---
+++ PiCross/Domain/Facade/Editing/IPuzzleChecker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PiCross.Cells;
+using PiCross.DataStructures;
+using PiCross.Game;
+
+namespace PiCross.Facade.Editing
+{
+    public interface IPuzzleChecker
+    {
+        void FindAmbiguities( Constraints columnConstraints, Constraints rowConstraints, Cell<IGrid<bool>> output );
+    }
+}
