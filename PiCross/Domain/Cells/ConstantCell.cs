ef3ae8792f7be7c277d3b046259091cf6197a592
Frederic Vogels <frederic.vogels@gmail.com>
2015-06-22 13:50:32 +0200

    Added ConstantCell

---
+++ PiCross/Domain/Cells/ConstantCell.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiCross.Cells
+{
+    internal class ConstantCell<T> : Cell<T>
+    {
+        public ConstantCell( T value )
+            : base( value )
+        {
+            // NOP
+        }
+
+        public override T Value
+        {
+            get
+            {
+                return base.Value;
+            }
+            set
+            {
+                throw new InvalidOperationException( "Cannot change value of ConstantCell" );
+            }
+        }
+
+        public override void Refresh()
+        {
+            // NOP
+        }
+    }
+}
