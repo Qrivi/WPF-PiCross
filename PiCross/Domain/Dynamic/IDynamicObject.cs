cec1ddb38c69860dd96548a37cf0becf80fb34f0
Frederic Vogels <frederic.vogels@gmail.com>
2015-06-22 23:57:26 +0200

    Beginnings of dynamic objects

---
+++ PiCross/Domain/Dynamic/IDynamicObject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PiCross.Cells;
+
+namespace PiCross.Dynamic
+{
+    public interface IDynamicObject
+    {
+        IEnumerable<IDynamicProperty> Properties { get; }
+
+        Cell<object> this[IDynamicProperty property] { get; }
+    }
+
+    public interface IDynamicProperty
+    {
+        string Name { get; }
+    }
+
+    public interface IDynamicObjectGroup
+    {
+        IEnumerable<IDynamicObject> Properties { get; }
+
+        IEnumerable<object> PropertyValues( IDynamicProperty property );
+    }
+
+    public interface IFilter
+    {
+        bool Accept( IDynamicObject obj );
+    }
+}
