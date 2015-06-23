76e2248781d4d05c200402b49175b51b8b3ebfb1
Frederic Vogels <frederic.vogels@gmail.com>
2015-06-23 15:04:14 +0200

    Fixed code - added missing usings

--- PiCross/Domain/Facade/IO/Library.cs
+++ PiCross/Domain/Facade/IO/Library.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PiCross.Game;
 
 namespace PiCross.Facade.IO
 {
--- PiCross/Domain/Facade/IO/LibraryIO.cs
+++ PiCross/Domain/Facade/IO/LibraryIO.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PiCross.DataStructures;
+using PiCross.Game;
 
 namespace PiCross.Facade.IO
 {
