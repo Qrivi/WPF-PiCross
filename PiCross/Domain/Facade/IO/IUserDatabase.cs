9efe97cb86fd8291dd62a7b88b8300439556c3e2
Frederic Vogels <frederic.vogels@gmail.com>
2015-06-25 17:56:31 +0200

    Added IUserDatabase.cs

---
+++ PiCross/Domain/Facade/IO/IUserDatabase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PiCross.Game;
+
+namespace PiCross.Facade.IO
+{
+    public interface IUserDatabase
+    {
+        IUserProfile GetUserProfile( string name );
+    }
+
+    public interface IUserProfile
+    {
+        IUserPuzzleInformation PuzzleInformation { get; }
+
+        string Name { get; }
+    }
+
+    public interface IUserPuzzleInformation
+    {
+        IUserPuzzleInformationEntry this[Puzzle puzzle] { get; set; }
+    }
+
+    public interface IUserPuzzleInformationEntry
+    {
+        TimeSpan BestTime { get; }
+    }
+}
