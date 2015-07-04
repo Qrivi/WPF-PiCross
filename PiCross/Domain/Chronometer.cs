1193d70ed8fe9712639073755b9ef27132e827af
Frederic Vogels <frederic.vogels@gmail.com>
2015-07-04 13:40:22 +0200

    Added Chronometer

---
+++ PiCross/Domain/Chronometer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiCross
+{
+    public class Chronometer
+    {
+        private DateTime lastTick;
+
+        private bool started;
+
+        private TimeSpan totalTime;
+
+        public Chronometer()
+        {
+            started = false;
+        }
+
+        public void Start()
+        {
+            started = true;
+            lastTick = DateTime.Now;
+        }
+
+        public void Pause()
+        {
+            started = false;
+        }
+
+        public void Tick()
+        {
+            if ( started )
+            {
+                var now = DateTime.Now;
+                var delta = now - lastTick;
+                lastTick = now;
+
+                totalTime += delta;
+            }
+        }
+
+        public TimeSpan TotalTime
+        {
+            get
+            {
+                return totalTime;
+            }
+        }
+    }
+}
