f81ef87a95030fe00eefef153bb3c9be345146a6
Frederic Vogels <frederic.vogels@gmail.com>
2016-02-13 22:23:41 +0100

    Grouped game data related classes in same file

--- PiCross/Domain/PiCross/IGameData.cs
+++ PiCross/Domain/PiCross/IGameData.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Cells;
 
 namespace PiCross
 {
@@ -11,5 +12,48 @@ namespace PiCross
         IPuzzleLibrary PuzzleLibrary { get; }
 
         IPlayerDatabase PlayerDatabase { get; }
+    }
+
+    public interface IPlayerDatabase
+    {
+        IPlayerProfile this[string name] { get; }
+
+        IPlayerProfile CreateNewProfile( string name );
+
+        bool IsValidPlayerName( string name );
+
+        IList<string> PlayerNames { get; }
+    }
+
+    public interface IPlayerProfile
+    {
+        IPlayerPuzzleInformation PuzzleInformation { get; }
+
+        string Name { get; }
+    }
+
+    public interface IPlayerPuzzleInformation
+    {
+        IPlayerPuzzleInformationEntry this[IPuzzleLibraryEntry libraryEntry] { get; }
+    }
+
+    public interface IPlayerPuzzleInformationEntry
+    {
+        // TODO Simplify
+        Cell<TimeSpan?> BestTime { get; }
+    }
+
+    public interface IPuzzleLibrary
+    {
+        IList<IPuzzleLibraryEntry> Entries { get; }
+
+        IPuzzleLibraryEntry Create( Puzzle puzzle, string author );
+    }
+
+    public interface IPuzzleLibraryEntry
+    {
+        Puzzle Puzzle { get; set; }
+
+        string Author { get; }
     }
 }
