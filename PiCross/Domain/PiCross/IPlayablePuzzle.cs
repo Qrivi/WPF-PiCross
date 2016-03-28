aaaa8d9d93f98d10a53a113df4885a0176f4a5e5
Frederic Vogels <frederic.vogels@gmail.com>
2016-03-28 16:12:35 +0200

    Added comments

--- PiCross/Domain/PiCross/IPlayablePuzzle.cs
+++ PiCross/Domain/PiCross/IPlayablePuzzle.cs
@@ -9,35 +9,85 @@ using Cells;
 
 namespace PiCross
 {
+    /// <summary>
+    /// Represents a playable puzzle.
+    /// A playable puzzle offers the following functionality:
+    /// <list type="bullet">
+    /// <item>
+    /// <description>
+    /// A modifiable grid. Each square in the grid can take three values: UNKNOWN, FILLED and EMPTY (<see cref="Square" />.)
+    /// The goal is for the player to fill in the grid correctly.
+    /// </description>
+    /// </item>
+    /// <item>
+    /// <description>
+    /// Row and colum constraints. These are all readonly.
+    /// </description>
+    /// </item>
+    /// </list>
+    /// </summary>
     public interface IPlayablePuzzle
     {
+        /// <summary>
+        /// Grid of IPlayablePuzzleSquares.
+        /// </summary>
         IGrid<IPlayablePuzzleSquare> Grid { get; }
-
+        
+        /// <summary>
+        /// Column constraints.
+        /// </summary>
         ISequence<IPlayablePuzzleConstraints> ColumnConstraints { get; }
 
+        /// <summary>
+        /// Row constraints.
+        /// </summary>
         ISequence<IPlayablePuzzleConstraints> RowConstraints { get; }
 
+        /// <summary>
+        /// Contains true if the Grid contains nothing but correct FILLED and EMPTY values,
+        /// false otherwise.
+        /// </summary>
         Cell<bool> IsSolved { get; }
     }
 
     public interface IPlayablePuzzleSquare
     {
+        /// <summary>
+        /// Contents of the square.
+        /// </summary>
         Cell<Square> Contents { get; }
 
+        /// <summary>
+        /// Position of the square in the grid.
+        /// </summary>
         Vector2D Position { get; }
     }
 
     public interface IPlayablePuzzleConstraints
     {
+        /// <summary>
+        /// Series of values.
+        /// </summary>
         ISequence<IPlayablePuzzleConstraintsValue> Values { get; }
 
+        /// <summary>
+        /// Contains true if the corresponding row or column satisfies
+        /// the pattern described by this object.
+        /// </summary>
         Cell<bool> IsSatisfied { get; }
     }
 
     public interface IPlayablePuzzleConstraintsValue
     {
+        /// <summary>
+        /// Actual value.
+        /// </summary>
         int Value { get; }
 
+        /// <summary>
+        /// Contains true if the corresponding row or column
+        /// satisfies this particular value.
+        /// </summary>
         Cell<bool> IsSatisfied { get; }
     }
 }
--- PiCross/Domain/PiCross/PiCrossFacade.cs
+++ PiCross/Domain/PiCross/PiCrossFacade.cs
@@ -9,8 +9,16 @@ using PiCross;
 
 namespace PiCross
 {
+    /// <summary>
+    /// Centralized access point to the domain classes.
+    /// </summary>
     public class PiCrossFacade
     {
+        /// <summary>
+        /// Creates a IGameData object that contains no puzzles or players.
+        /// Everything is kept in memory, i.e. data will never be persisted to file.
+        /// </summary>
+        /// <returns>An IGameData object.</returns>
         public IGameData CreateEmptyGameData()
         {
             var data = new InMemoryDatabase( InMemoryDatabase.PuzzleLibrary.CreateEmpty(), InMemoryDatabase.PlayerDatabase.CreateEmpty() );
@@ -18,20 +26,38 @@ namespace PiCross
             return new GameDataAdapter( data );
         }
 
+        /// <summary>
+        /// Creates dummy game data. Feel free to adapt <see cref="DummyData" /> to suit your needs.
+        /// This data is kept in memory; in other words, it will never be persisted to file.
+        /// </summary>
+        /// <returns>An IGameData object.</returns>
         public IGameData CreateDummyGameData()
         {
             return new GameDataAdapter( DummyData.Create() );
         }
 
+        /// <summary>
+        /// Loads data from the given file.
+        /// When the data is modified, changes are automatically written to the file.
+        /// </summary>
+        /// <param name="path">Path to the file.</param>
+        /// <returns>An IGameData object.</returns>
         public IGameData LoadGameData( string path )
         {
             var archive = new AutoCloseGameDataArchive( path );
-            // var gameData = new ArchivedGameData( archive );
             var gameData = new GameDataAdapter( new ArchiveDatabase( path ) );
 
             return gameData;
         }
 
+        /// <summary>
+        /// Creates a puzzle editor. This object offers
+        /// all functionality related to the creation and modification
+        /// of puzzles.
+        /// </summary>
+        /// <param name="puzzle">Puzzle to be edited. Note that this object cannot be changed; the editor
+        /// uses this puzzle solely as an initial state.</param>
+        /// <returns>An IPuzzleEditor object.</returns>
         public IPuzzleEditor CreatePuzzleEditor( Puzzle puzzle )
         {
             var editorGrid = EditorGrid.FromPuzzle( puzzle );
@@ -47,6 +73,12 @@ namespace PiCross
             return new StepwiseSolver( solverGrid );
         }
 
+        /// <summary>
+        /// Creates a playable puzzle. This object
+        /// offers all functionality related to the solving of a puzzle.
+        /// </summary>
+        /// <param name="puzzle">Puzzle to be solved.</param>
+        /// <returns>An IPlayablePuzzle object.</returns>
         public IPlayablePuzzle CreatePlayablePuzzle( Puzzle puzzle )
         {
             return new PlayablePuzzle( columnConstraints: puzzle.ColumnConstraints, rowConstraints: puzzle.RowConstraints );
