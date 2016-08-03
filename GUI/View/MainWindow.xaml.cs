f1aa2bbe524ee08638ff19cb8ea35a85f47ff5d8
Kristof Dewilde <me@krivi.be>
2016-08-03 04:44:17 +0200

    Moved app initialization to App.xaml.cs
    rather than MainWindow.xaml.cs.
    
    ¯\_(ツ)_/¯

--- GUI/View/MainWindow.xaml.cs
+++ GUI/View/MainWindow.xaml.cs
@@ -1,14 +1,15 @@
 ﻿using System.Windows;
 
-namespace GUI {
+namespace GUI
+{
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow() {
+        public MainWindow()
+        {
             InitializeComponent();
-            PiCrossGUI.DataContext = new PiCrossViewModel();
         }
     }
 }
